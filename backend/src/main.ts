80cd0331f2757e2e9d9d4be68623f2888bfb7867
abela-a <abelardhana96@gmail.com>
2024-11-28 11:18:48 +0800

    [BE] feat(swagger): add Swagger integration for API documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+function swaggerInit(app) {
+  const config = new DocumentBuilder()
+    .setTitle('YouApp API')
+    .setDescription('YouApp API Challenge by @abela-a')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+  swaggerInit(app);
 
   await app.listen(process.env.PORT ?? 3000);
-
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
